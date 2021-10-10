e2a6a86f8c713c5c8663c5f933d4182b24d6ccd3
sanuwijay94 <sanuwijayarathne@gmail.com>
2021-10-10 07:50:10 +0530

    Updated home to add field details

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,165 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, Keyboard, Text, TextInput, TouchableOpacity, View, Button } from 'react-native'
+import styles from './styles';
+import { firebase } from '../../firebase/config'
+import Modal from "react-native-modal";
 
-export default function HomeScreen(props) {
+export default function HomeScreen(props, {navigation}) {
+
+    
+    const [cropName, setCropName] = useState('')
+    const [area, setArea] = useState('')
+    const [fields, setfields] = useState([])
+
+    const fieldRef = firebase.firestore().collection('fields')
+    const userID = props.extraData.id
+
+    const [isModalVisible, setIsModalVisible] = React.useState(false);
+    const handleModal = () => setIsModalVisible(() => !isModalVisible);
+
+    const onLogoutPress = async() => {
+      
+        try {
+            await firebase
+            .auth()
+            .signOut()
+            
+             console.log('User signed out!');
+             navigation.navigate('Login')
+            
+        }catch(error){
+                alert(error)
+        } 
+    }
+
+    useEffect(() => {
+        fieldRef
+            .where("authorID", "==", userID)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot(
+                querySnapshot => {
+                    const newFields = []
+                    querySnapshot.forEach(doc => {
+                        const field = doc.data()
+                        field.id = doc.id
+                        newFields.push(field)
+                    });
+                    setfields(newFields)
+                },
+                error => {
+                    console.log(error)
+                }
+            )
+    }, [])
+
+    const onAddButtonPress = () => {
+        if (cropName && cropName.length > 0) {
+            const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+            const data = {
+                crop: cropName,
+                area: area,
+                authorID: userID,
+                createdAt: timestamp,
+            };
+            fieldRef
+                .add(data)
+                .then(_doc => {
+                    setCropName('')
+                    setArea('')
+                    Keyboard.dismiss()
+                    handleModal()
+                })
+                .catch((error) => {
+                    alert(error)
+                });
+        }
+    }
+
+    const onfieldItemPress = (field) => {}
+
+    const renderField = ({item}) => {
+        return (
+            <View style={styles.fieldContainer}>
+                <TouchableOpacity
+                    style={styles.fieldBox}
+                    onPress={() => onfieldItemPress(item)}>
+                    <Text style={styles.buttonText}>
+                        {item.crop}
+                    </Text>
+                    <Text style={styles.buttonText}>
+                        {item.area}ft
+                    </Text>
+                </TouchableOpacity>
+                
+            </View>
+        
+        )
+    } 
+
     return (
-        <View>
-            <Text>Home Screen</Text>
+        <View style={styles.container}>
+            <TouchableOpacity 
+                style={styles.logoutButton}
+                onPress={() => onLogoutPress()}>
+                <Text style={styles.buttonText}>Log out</Text>
+            </TouchableOpacity>
+            {/* <View style={styles.formContainer}> */}
+             {/*    <TextInput
+                    style={styles.input}
+                    placeholder='Add new field'
+                    placeholderTextColor="#aaaaaa"
+                    onChangeText={(text) => setCropName(text)}
+                    value={cropName}
+                    underlineColorAndroid="transparent"
+                    autoCapitalize="none"
+                /> */}
+                
+            {/* </View> */}
+            { fields && (
+                <View style={styles.listContainer}>
+                    <TouchableOpacity style={styles.button} onPress={handleModal}>
+                        <Text style={styles.buttonText1}>+</Text>
+                    </TouchableOpacity>
+                    <Modal isVisible={isModalVisible}>
+                        <View style={{ flex: 1 }}>
+                            <Text style={styles.buttonText2}>Crop Name</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Add Crop Name'
+                                placeholderTextColor="#aaaaaa"
+                                onChangeText={(crop) => setCropName(crop)}
+                                value={cropName}
+                                underlineColorAndroid="transparent"
+                                autoCapitalize="none"
+                            />
+                            <Text style={styles.buttonText2}>Area</Text>
+                            <TextInput
+                                style={styles.input}
+                                placeholder='Add Area'
+                                placeholderTextColor="#aaaaaa"
+                                onChangeText={(area) => setArea(area)}
+                                value={area}
+                                underlineColorAndroid="transparent"
+                                autoCapitalize="none"
+                            />
+                            <View style={styles.formContainer}>
+                                <TouchableOpacity style={styles.savebutton} onPress={() => onAddButtonPress()}>
+                                    <Text style={styles.buttonText}>Save</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.cancelbutton} onPress={handleModal}>
+                                    <Text style={styles.buttonText}>Cancel</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </Modal>
+                    <FlatList
+                        data={fields}
+                        renderItem={renderField}
+                        keyExtractor={(item) => item.id}
+                        removeClippedSubviews={true}
+                    />
+                </View>
+            )}
         </View>
     )
 }
--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,19 +1,109 @@
 import { StyleSheet } from 'react-native';
 
+
 export default StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center'
     },
+    formContainer: {
+        flexDirection: 'row',
+        height: 20,
+        marginTop: 40,
+        marginBottom: 50,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 5,
+        paddingRight: 5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     input: {
-        height: 48,
-        borderRadius: 5,
+        height: 50,
+        borderRadius: 50,
         overflow: 'hidden',
         backgroundColor: 'white',
+        paddingLeft: 38,
+        marginTop: 10
+    },
+    button: {
+        height: 80,
+        borderRadius: 100,
+        backgroundColor: 'black',
+        width: 120,
+        alignItems: "center",
+        justifyContent: 'center',
+        alignSelf: 'center',
+        right: 0,
+        
+    },
+    savebutton: {
+        height: 60,
+        borderRadius: 80,
+        backgroundColor: 'black',
+        width: 120,
+        marginRight: 30,
+        alignItems: "center",
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        
+        
+    },
+    cancelbutton: {
+        height: 60,
+        borderRadius: 80,
+        backgroundColor: 'black',
+        width: 120,
+        marginLeft: 30,
+        alignItems: "center",
+        justifyContent: 'center',
+        alignSelf: 'flex-start',
+        
+        
+    },
+    logoutButton: {
+        height: 55,
+        borderRadius: 50,
+        backgroundColor: 'black',
+        width: 500,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    fieldBox: {
+        height: 150,
+        borderRadius: 20,
+        backgroundColor: '#808080',
+        width: 250,
+        alignItems: "center",
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
         marginTop: 10,
         marginBottom: 10,
-        marginLeft: 30,
-        marginRight: 30,
-        paddingLeft: 16
+        fontSize: 20
+    },
+    buttonText1: {
+        color: 'white',
+        fontSize: 34
+    },
+    buttonText2: {
+        color: 'white',
+        fontSize: 28
+    },
+    listContainer: {
+        marginTop: 20,
+        color: 'black',
+        padding: 20,
+    },
+    fieldContainer: {
+        marginTop: 16,
+        borderBottomColor: 'black',
+        borderBottomWidth: 1,
+        paddingBottom: 16
+    },
+    setCropName: {
+        fontSize: 20,
+        color: '#333333'
     }
 })
